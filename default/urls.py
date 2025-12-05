3bc51f462cc5d0e03af9995f67dff0d3dbcf0519
修正戴 <owencarjoker@gmail.com>
2025-12-05 11:45:03 +0800

    25/12/05 完成投票主題、查看投票主題、投票功能

--- default/urls.py
+++ default/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("" , poll_list), #乎較位於views中的東西(ex.poll_list傳送req)
     path("list",Polllist.as_view(), name='poll_list'), #用name 為各種路徑取一個名子 
     path("<int:pk>/",PollView.as_view(), name = 'poll_view'), #用小於大於標示要變動的參數,pk是一個參數  ;#用name 為各種路徑取一個名子，可與poll_detail第12行的url合併使用
-    path('<int:oid>/vote/',PollVote.as_view(), name= 'poll_vote')
+    path('<int:oid>/vote/',PollVote.as_view(), name= 'poll_vote'),
 ]
+
+#<int:pk>中，pk是預設的變數名稱，無須額外跟listview說，但如果要自訂變數名稱的話要記的跟listview說(在default/views.py中有用到)         
--- default/views.py
+++ default/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Poll, Option 
 from django.views.generic import ListView, DetailView, RedirectView #一個是獲取資料列表，一個是獲取詳細的資料;RedirectView 仔仔入夜麵食重新導向一次(把變更過的票數重新縣市一次)
- 
+from django.urls import reverse #給他一組路徑規則的名城她會反推一次路徑
+
 # Create your views here.
 def poll_list(req):   #，定義並接收來自url傳來的req
     polls = Poll.objects.all()   #把投票紀錄塞入Poll
@@ -9,7 +10,8 @@ def poll_list(req):   #，定義並接收來自url傳來的req
     #render(收到回應後， "用個頁面範本去顯示"， {"從poll_list中" 從所有在(:)polls中的 , 第二個傳去的資料'msg':'hellow' })
 
 #資料模型Poll建在default/mosels.py
-class Polllist(ListView):
+
+class Polllist(ListView):   #把listview的功能繼承給Polllist
     model = Poll  #定義資料來源要從哪個資料模型索要
 
     #尋找要顯示的頁面範本的規則(list view):[應用程式名稱/資料模型_list.html]
@@ -22,11 +24,23 @@ class PollView(DetailView):  #DetailView 會預設用urld中設定的pk參數去
 
 #def poll_view(req, pk):
     #poll = Poll.object.get(id = pk)   (前備知識:每個資料在產生時會戴有一個屬於自己的id，從第一筆資料1、第二筆2....)此兩行是DetailView預設會做的事:找出資料id與pk相同的資料並且用於顯示
-    def get_context_data(self, **kwargs):  #額外獲取要傳給頁面範本的資料(解決有些資料不再Poll中)        
-        ctx =  super().get_context_data(**kwargs) #ctx是一本字典，裡面裝有DetailView獲取要傳給範本的資料和他沒獲取到的
+    def get_context_data(self, **kwargs):  #額外獲取要傳給頁面範本的資料(解決有些資料不再Poll中)(產生一本字典)        
+        ctx =  super().get_context_data(**kwargs) #ctx是一本字典，裡面裝有DetailView獲取要傳給範本的資料和他沒獲取到的;[super()]從PollView的副(父?)類別中先獲取執行get_context_data()的方法，並將獲得的東西存入字典
         #option_list = Option.objects.filter(poll_id=self.object.id)  #object:紀錄獲取資料各種屬性(id就是其中一種)
         #ctx['option_list'] = option_list
         ctx['option_list'] = Option.objects.filter(poll_id=self.object.id)  #化簡上兩行
         return ctx #記得把加工後的資料傳回去給目標的頁面範本使用
-class PollVote(RedirectView):
-    pass
+class PollVote(RedirectView):  #副屬性:用於重新導向頁面(導項不同或相同的頁面)
+    #redirect_url = "https://www.google.com/"  用於單一導向(只能導向同一個網址)
+    def get_redirect_url(self, *args, **kwargs):
+        option = Option.objects.get(id = self.kwargs['oid'])       #條件撈一筆紀錄>>get();撈多比>>filter()
+        option.votes += 1 #option.votes = option.votes +1  ;要記得將剛新過的數值存入資料庫
+        option.save() #把更改過的檔案存入資料庫 
+        #return "/poll/{}/".format(option.poll_id)
+        #return f"/poll/{option.poll_id}"
+        #return reverse('poll_view', args=[option.poll_id]) #用鳴子為poll_view的網址反推回去;args=[option.poll_id]把變更過的變數改入poll_id
+        return reverse('poll_view', kwargs={'pk':option.poll_id})   #效果同上
+    
+
+
+
